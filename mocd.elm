b03c850c0463e6f0b6e43a1f1096223a9006ecde
Francesco Anselmo <francesco.anselmo@gmail.com>
2021-04-10 16:37:06 +0100

    initial code

---
+++ mocd.elm
@@ -0,0 +1,219 @@
+<console version="1" appVersion="1.0.0.560">
+  <model fps="30" isStagesAccessRestricted="False">
+    <stages>
+      <stage name="Ministry of Collective Data" id="33a2d833-71b2-44d5-b00f-525a53790480" width="306" height="40" media="-1" merge="overwrite" hidden="False" locked="False">
+        <live intensity="1" crossfadeMs="2000" fadePosition="0" mixEffect="CrossFade" mixEffectPositionTransform="sigmoid" speed="0.928">
+          <color red="255" green="255" blue="255" />
+          <zone1 media="0" />
+          <zone2 media="0" />
+        </live>
+        <background path="data/led_matrix_cropped.png" />
+        <fixtures>
+          <fixture group="RGB01" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="0" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="159.173373078359" y="20.8282478153062" />
+                <point x="172.959087330152" y="20.8282478153062" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB02" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="1" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="172.959087330152" y="21.7643381102696" />
+                <point x="159.173373078359" y="21.7643381102696" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB03" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="2" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="159.173373078359" y="22.700428405233" />
+                <point x="172.959087330152" y="22.700428405233" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB04" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="3" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="172.959087330152" y="23.6365187001963" />
+                <point x="159.173373078359" y="23.6365187001963" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB05" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="4" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="159.173373078359" y="24.5726089951597" />
+                <point x="172.959087330152" y="24.5726089951597" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB06" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="5" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="172.959087330152" y="25.508699290123" />
+                <point x="159.173373078359" y="25.508699290123" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB07" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="6" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="159.173373078359" y="26.4447895850864" />
+                <point x="172.959087330152" y="26.4447895850864" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB08" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="7" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="172.959087330152" y="27.3808798800498" />
+                <point x="159.173373078359" y="27.3808798800498" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB09" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="8" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="159.173373078359" y="28.3169701750131" />
+                <point x="172.959087330152" y="28.3169701750131" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB10" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="9" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="172.959087330152" y="29.2530604699765" />
+                <point x="159.173373078359" y="29.2530604699765" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB11" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="10" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="159.173373078359" y="30.1891507649398" />
+                <point x="172.959087330152" y="30.1891507649398" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB12" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="11" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="172.959087330152" y="31.1252410599032" />
+                <point x="159.173373078359" y="31.1252410599032" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB13" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="12" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="159.173373078359" y="32.0613313548666" />
+                <point x="172.959087330152" y="32.0613313548666" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB14" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="13" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="172.959087330152" y="32.9974216498299" />
+                <point x="159.173373078359" y="32.9974216498299" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB15" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="14" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="159.173373078359" y="33.9335119447933" />
+                <point x="172.959087330152" y="33.9335119447933" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB16" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="15" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="172.959087330152" y="34.8696022397566" />
+                <point x="159.173373078359" y="34.8696022397566" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB17" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="16" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="159.173373078359" y="35.80569253472" />
+                <point x="172.959087330152" y="35.80569253472" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB18" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="17" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="172.959087330152" y="36.7417828296834" />
+                <point x="159.173373078359" y="36.7417828296834" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB19" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="18" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="159.173373078359" y="37.6778731246467" />
+                <point x="172.959087330152" y="37.6778731246467" />
+              </points>
+            </geometry>
+          </fixture>
+          <fixture group="RGB20" group2="" ledType="RGB" dmxProtocol="ArtNet" startUniverse="19" startAddress="0" breakUniverseAfter="17">
+            <geometry nbLeds="17" type="line" loop="False" rotation="0">
+              <points>
+                <point x="172.959087330152" y="38.6139634196101" />
+                <point x="159.173373078359" y="38.6139634196101" />
+              </points>
+            </geometry>
+          </fixture>
+        </fixtures>
+        <schedule stopDuringDaylight="False">
+          <playlists />
+        </schedule>
+      </stage>
+    </stages>
+    <media audioMuted="True">
+      <slots>
+        <slot id="0" name="Screen capture">
+          <source relativePath="@screencapture&lt;node x=&quot;225&quot; y=&quot;355&quot; width=&quot;918&quot; height=&quot;118&quot; fps=&quot;25&quot; /&gt;" type="2">
+            <config>
+              <root size2="0" size3="0" size4="0" size5="0" size6="0" toggle1="False" toggle2="False">
+                <remote />
+              </root>
+            </config>
+          </source>
+        </slot>
+      </slots>
+    </media>
+    <output>
+      <artnet sendPartialFrames="False" sendArtSync="False">
+        <universes />
+      </artnet>
+      <sAcn outputUniverseOffset="0" optimizeFrames="True" priority="100" sendSync="False">
+        <universes />
+      </sAcn>
+      <kinet>
+        <universes />
+      </kinet>
+    </output>
+    <input />
+    <ledPreviewColors>
+      <color type="White" intensity="1" temperature="6600" />
+      <color type="WhiteCool" intensity="0.33" temperature="9000" />
+      <color type="WhiteNeutral" intensity="0.33" temperature="6600" />
+      <color type="WhiteWarm" intensity="0.33" temperature="2700" />
+      <color type="last" intensity="1" temperature="6600" />
+    </ledPreviewColors>
+  </model>
+  <view displayArtnetHexadecimal="False">
+    <previews />
+    <stages>
+      <stage ID="33a2d833-71b2-44d5-b00f-525a53790480">
+        <grid isVisible="False" squareWidth="17" squareHeight="20" squareOffsetX="4" squareOffsetY="0" />
+      </stage>
+    </stages>
+  </view>
+</console>
---
+++ mocd_ledmatrix.pde
@@ -0,0 +1,74 @@
+/**
+ * MOCD matrix 
+ */
+
+int pixelSize=2;
+PGraphics pg;
+PImage matrixImage;
+boolean showMatrix = true;
+
+void setup(){
+  size(918, 120);
+  frameRate(50);
+  matrixImage = loadImage("led_matrix_cropped.png"); 
+  // create a buffered image for storing the plasma effect
+  pg = createGraphics(918, 120);
+  colorMode(RGB);
+  noSmooth();
+}
+
+void draw()
+{
+  float  xc = 1000;
+
+  int timeDisplacement = frameCount;
+
+  // No need to do this math for every pixel
+  float calculation1 = sin( radians(timeDisplacement * 0.06));
+  float calculation2 = sin( radians(timeDisplacement * -0.09));
+  
+  // Output into a buffered image for reuse
+  pg.beginDraw();
+  pg.loadPixels();
+
+  // Plasma algorithm
+  for (int x = 0; x < pg.width; x++, xc += pixelSize)
+  {
+    float  yc    = 1000;
+    float s1 = 128 + 128 * sin(radians(xc) * calculation1 );
+
+    for (int y = 0; y < pg.height; y++, yc += pixelSize)
+    {
+      float s2 = 128 + 128 * sin(radians(yc) * calculation2 );
+      float s3 = 128 + 128 * sin(radians((xc + yc + timeDisplacement * 5) / 2));  
+      //float s  = (s1+ s2 + s3) / 3;
+      float s = s2;
+      //pg.pixels[x+y*pg.width] = color(s, 255 - s / 2.0, 255);
+      //pg.pixels[x+y*pg.width] = color(s, 255 - s / 2.0, 255);
+      pg.pixels[x+y*pg.width] = color(s, 0, 255);
+    }
+  }   
+  pg.updatePixels();
+  pg.endDraw();
+
+  // display the results
+  image(pg,0,0,width,height); 
+
+  if (showMatrix == true) {
+     tint(255, 127);  // Display at half opacity
+     image(matrixImage, 0, 0, width, height);
+     tint(255, 127);  // Display at half opacity
+  }
+
+}
+
+void keyReleased() {
+  // save image
+  if (key=='s' || key=='S') saveFrame(timestamp()+".png"); 
+  if (key=='m' || key=='M') showMatrix = !showMatrix; 
+}
+
+String timestamp() {
+  //return String.format("%1$ty%1$tm%1$td_%1$tH%1$tM%1$tS", Calendar.getInstance());
+  return  str(year())+"-"+str(month())+"-"+str(day())+"_"+str(hour())+"-"+str(minute())+"-"+str(millis());
+}
